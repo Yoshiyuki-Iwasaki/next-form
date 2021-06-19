0cebab57cc49464161b20b98639a13693eda3cf7
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-06-19 13:00:45 +0900

    [add] source

---
+++ pages/api/contact.tsx
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+import { getMaxListeners } from "process";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const { name, mail, content, token } = req.body;
+  console.log("name", name, "mail", mail, "content", content, "token", token);
+
+  const human = await validateHuman(token);
+  if (!human) {
+    res.status(400);
+    res.json({ errors: ["bot"] });
+  }
+  console.log("human", human);
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_ADDRESS,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+  const output_subject = `Contact form submission from ${name}`;
+  const output_html = `<p>メールが送信されました。ご回答までお待ちください。</p>
+      <p><strong>Name: </strong>${name}</p>
+      <p><strong>Mail: </strong>${mail}</p>
+      <p><strong>Content: </strong>${content}</p>
+  `;
+
+  try {
+    const emailRes = await transporter.sendMail({
+      from: mail,
+      to: `${mail},armada3524@gmail.com`,
+      subject: output_subject,
+      html: output_html,
+    });
+    console.log("emailRes", emailRes.messageId);
+  } catch (error) {
+    console.log("errorだよ", error);
+  }
+
+  res.status(200).json(req.body);
+}
+
+async function validateHuman(token: any) {
+  const secret = "6LfeXzsbAAAAAJ2XSd50WHtufM3PWW7RsprZrf7d";
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    { method: "POST" }
+  );
+  const data = await response.json();
+  console.log("data", data);
+
+  return data.success;
+}
